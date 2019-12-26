788cf1a8fe2622d743b47fda307e4b913636bc61
Livia de Castro e Mello <lcastroemello@gmail.com>
2019-12-26 19:37:58 +0100

    bundle

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {useSelector, useDispatch} from "react-redux";
-import { Route, BrowserRouter, Link } from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 
 import {saveCrisis, addNewCrisis} from './actions';
 
@@ -25,48 +25,47 @@ export default function App()  {
         <BrowserRouter>
             <div>
                 {addCrisisIsVisible && 
-                <div style={{background: "#f5fcef", margin:0}}>
-                    <h1 style={{fontSize: '4rem', textAlign: 'center', color: "#334431",
-                        fontFamily: "Lacquer, sans-serif"}} >Enter your crisis info and click on save!</h1>
-                    <h2 style={{fontSize: '3rem'}}>1) What type of crisis did you have?</h2>
-                    <br/>
-                    <Types/>
-                    <hr />
-                    <h2 style={{fontSize: '4rem'}} >2) What was the context?</h2>
-                    <Context/>
-                    <hr />
-                    <h2 style={{fontSize: '4rem'}} >3)How long did your crisis last?</h2>
-                    <Duration/>
-                    <hr />
-                    <button
-                        style = {{ color: 'white',
-                            backgroundColor: '#3f87a6',
-                            fontSize: '3rem',
-                            margin: 'none',
-                            padding: '0.5rem',
-                            fontWeight: 'bold'}}
-                        className="button"
-                        onClick={() => saveCrisisInfo()}
-                    >Save this crisis</button>
-                </div>
+                    <div style={{background: "#f5fcef", margin:0}}>
+                        <h1 style={{fontSize: '4rem', textAlign: 'center', color: "#334431",
+                            fontFamily: "Lacquer, sans-serif"}} >Enter your crisis info and click on save!</h1>
+                        <h2 style={{fontSize: '3rem'}}>1) What type of crisis did you have?</h2>
+                        <br/>
+                        <Types/>
+                        <hr />
+                        <h2 style={{fontSize: '4rem'}} >2) What was the context?</h2>
+                        <Context/>
+                        <hr />
+                        <h2 style={{fontSize: '4rem'}} >3)How long did your crisis last?</h2>
+                        <Duration/>
+                        <hr />
+                        <button
+                            style = {{ color: 'white',
+                                backgroundColor: '#3f87a6',
+                                fontSize: '3rem',
+                                margin: 'none',
+                                padding: '0.5rem',
+                                fontWeight: 'bold'}}
+                            className="button"
+                            onClick={() => saveCrisisInfo()}
+                        >Save this crisis</button>
+                    </div>
                 }
                 {!addCrisisIsVisible &&
-            <div>
-                <h1 style={{fontSize: '3rem'}} >Your crisis was registered on our system!</h1>
-                <button
-                    style = {{ color: 'white',
-                        backgroundColor: '#3f87a6',
-                        fontSize: '3rem',
-                        margin: 'none',
-                        padding: '0.5rem',
-                        fontWeight: 'bold'}}
-                    className="button"
-                    onClick={() => dispatch(addNewCrisis())}
-                >Back to crisis page</button>
+                     <div>
+                         <h1 style={{fontSize: '3rem'}} >Your crisis was registered on our system!</h1>
+                         <button
+                             style = {{ color: 'white',
+                                 backgroundColor: '#3f87a6',
+                                 fontSize: '3rem',
+                                 margin: 'none',
+                                 padding: '0.5rem',
+                                 fontWeight: 'bold'}}
+                             className="button"
+                             onClick={() => dispatch(addNewCrisis())}
+                         >Back to crisis page</button>
                 
-            </div>
+                     </div>
                 }
- 
             </div>
         </BrowserRouter>
     );
