8378638608bff294561567d258c861a1d5fbb723
Livia de Castro e Mello <lcastroemello@gmail.com>
2019-12-26 19:50:15 +0100

    bundle deletado

--- index.js
+++ index.js
@@ -32,27 +32,27 @@ app.use(compression());
 
 //-------------------FormData API----------------------
 
-var multer = require("multer");
-var uidSafe = require("uid-safe");
-var path = require("path");
+// var multer = require("multer");
+// var uidSafe = require("uid-safe");
+// var path = require("path");
 
-var diskStorage = multer.diskStorage({
-    destination: function(req, file, callback) {
-        callback(null, __dirname + "/uploads");
-    },
-    filename: function(req, file, callback) {
-        uidSafe(24).then(function(uid) {
-            callback(null, uid + path.extname(file.originalname));
-        });
-    }
-});
+// var diskStorage = multer.diskStorage({
+//     destination: function(req, file, callback) {
+//         callback(null, __dirname + "/uploads");
+//     },
+//     filename: function(req, file, callback) {
+//         uidSafe(24).then(function(uid) {
+//             callback(null, uid + path.extname(file.originalname));
+//         });
+//     }
+// });
 
-var uploader = multer({
-    storage: diskStorage,
-    limits: {
-        fileSize: 2097152
-    }
-});
+// var uploader = multer({
+//     storage: diskStorage,
+//     limits: {
+//         fileSize: 2097152
+//     }
+// });
 
 //--------------------BUNDLE.JS-------------------------
 
