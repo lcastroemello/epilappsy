9cd7867067a5420310858cda3d460e819b8bb3ca
Livia de Castro e Mello <lcastroemello@gmail.com>
2019-12-26 19:29:23 +0100

    fixin

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React from "react";
 import {useSelector, useDispatch} from "react-redux";
 import { Route, BrowserRouter, Link } from "react-router-dom";
 
